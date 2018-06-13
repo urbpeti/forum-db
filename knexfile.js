d3aea48dda32fc410d60742da254f91a8def2a7f
Peter <urbpeti@gmail.com>
2018-06-13 15:30:40 +0200

    :rocket: Add production connection fro deploy

--- knexfile.js
+++ knexfile.js
@@ -3,4 +3,8 @@ module.exports = {
     client: 'pg',
     connection: 'postgress://admin:test@localhost/community-forum'
   },
+  produciton: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+  },
 };
